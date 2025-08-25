14faca8fdea50dbe892c70c5f333d7bee126a2f8
FrancoFMV <francomilesqui@gmail.com>
2025-08-24 21:03:41 -0300

    Sprites de Carpi añadidos (Iddle, Run, Jump, Fall)

--- Jugador/jugador.gd
+++ Jugador/jugador.gd
@@ -2,8 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var appeared:bool = false
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	$animations.play("Appear")
 
 func _physics_process(delta: float):
 	##Add the gravity
@@ -19,3 +23,35 @@ func _physics_process(delta: float):
 	var direction = Input.get_axis("ui_left","ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	move_and_slide()
+	decide_animation()
+	
+func decide_animation():
+	#appear
+	if not appeared: return
+	# X Axis
+	if velocity.x == 0:
+		#iddle
+		$animations.play("Iddle")
+	elif velocity.x < 0:
+		#left
+		$animations.flip_h = true
+		$animations.play("Run")
+	elif velocity.x > 0:
+		#right
+		$animations.flip_h = false
+		$animations.play("Run")
+		
+	# Y Axis
+	if velocity.y > 0:
+		$animations.play("Fall")
+	elif velocity.y < 0:
+		$animations.play("Jump_up")
+
+
+func _on_animations_animation_finished() -> void:
+	if $animations.animation == "Appear":
+		appeared = true
